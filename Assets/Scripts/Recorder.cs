d89ec023b4465db26ecdeed42ffc67bbd9dca2d6
Ken Grimm <ken.grimm@welltok.com>
2016-06-30 23:19:12 -0600

    Buffer recording 3 lines at a time so that only complete movements are written.  Record using multiple files, trigger button creates a new file

--- Assets/Scripts/Recorder.cs
+++ Assets/Scripts/Recorder.cs
@@ -13,20 +13,39 @@ public class Recorder : MonoBehaviour {
 	public Transform[] parrotingTransforms;
 	
 	private StreamWriter streamWriter;
+	private int lastSequence = 0;
 
 	public void BeginRecording() {
 		InvokeRepeating("RecordFrame", 0, 0.011f);
 	}
 
 	void Start () {
-		int seq = 0;
-		string path = "recording_0.txt";
-		while(File.Exists(path = "recording_" + ++seq + ".txt")) {}
+		NewFile();
+	}
+
+	private void NewFile() {
+		if(streamWriter != null) {
+			streamWriter.Close();
+		}
+
+		string path = "recording_";
+		while(File.Exists(path + lastSequence++ + ".txt")) {}
 
 	  streamWriter = new StreamWriter(path);
 	}
+
+	void Update() {
+    SteamVR_Controller.Device controller = 
+			SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost));
+
+		if(controller.GetHairTriggerDown()) {
+			NewFile();
+		}
+	}
 	
 	void RecordFrame() {
+		string line = "";
+		
 		for (int i = 0; i < trackedTransforms.Length; i++) {
 			Transform tracked = trackedTransforms[i];
 			string name = names[i];
@@ -34,17 +53,21 @@ public class Recorder : MonoBehaviour {
 			parrotingTransforms[i].position = tracked.position;
 			parrotingTransforms[i].rotation = tracked.rotation;
 
-		  streamWriter.WriteLine(name + ':' + 
-				tracked.position.x + "," + 
-				tracked.position.y + "," +
-				tracked.position.z + "|" +
-				tracked.rotation.x + "," +
-				tracked.rotation.y + "," +
-				tracked.rotation.z + "," +
-				tracked.rotation.w
-				);
-
+			line += buildLine(name, tracked);
 		}
+		
+		streamWriter.WriteLine(line);
+	}
+
+	private string buildLine(string controllerName, Transform controller) {
+		return name + ':' + 
+			controller.position.x + "," + 
+			controller.position.y + "," +
+			controller.position.z + "|" +
+			controller.rotation.x + "," +
+			controller.rotation.y + "," +
+			controller.rotation.z + "," +
+			controller.rotation.w + "\n";
 	}
 
 	void Destroy () {
